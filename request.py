6d764a074da61d7a948a7334c3c3efd9a4200da5
lp-abuata <116755328+lp-abuata@users.noreply.github.com>
2024-01-02 17:10:52 +0400

    Add files via upload

--- request.py
+++ request.py
@@ -70,10 +70,12 @@ def get_request_object(tag=None):
     return {
         'headers': {'Content-Type': 'text/xml;charset=UTF-8'},
         'verify': False,
-        'url': f'http://62.231.248.100:8193/Connector/GenericConnector?type={tag}'
+        #'url': 'http://10.4.104.87:41409/ERecharge/ERSUpdateDealerInfo'
+        'url':'http://10.4.104.87:41409/ERecharge/ERSRegisterDealer'
+        #'url': f'http://10.4.104.86:8193/Connector/GenericConnector?type={tag}'
         #'url': 'http://62.231.248.100:8193/Connector/GenericConnector?type=GETEVCHRCARD'
         #'url': 'http://62.231.248.100:8193/ERecharge/ERSBalanceEnquiryAdapter'
-        #`'url': 'http://10.4.109.28:41409/ERecharge/ERSProductEnquiry'
+        #'url': 'http://10.4.109.28:41409/ERecharge/ERSProductEnquiry'
         #'url':'http://62.231.248.100:8193/ERecharge/ERSBalanceEnquiryAdapter?typeGETBALINFO'
     }
 		
@@ -90,22 +92,34 @@ def fill_xml_tags_with_data(xml_str: str, data: dict):
 def test(tag=None,filename=None,responseTag=None):
     request_object = get_request_object(tag)
     print("Request: ",request_object.get('url'))
+    print("==============================")
     request_file = get_template_file(filename)
-    #print("file",request_file)
+    print("Request: ",request_file)
+    print("==================================")
     request = requests.post(data=request_file, **request_object)
     print("headers: ",request.headers)
+    print("==============================")
     print("Request Content Type: ",request.headers.get("Content-Type", False) and str(request.headers.get("Content-Type", '')) )
+    print("==============================")
+    print(f"Response: {request.status_code}")
+    print("==============================")
     if request.status_code == 200:
         response = bytes(request.text, 'utf-8')
         print(response)
+        print("==============================")
         root = etree.fromstring(response)
         product_enquiry_response = parse_response(result_code="ResultCode", response_tag=responseTag, root=root)
         print("Parse: ",product_enquiry_response)
+        print("========================")
         product_enquiry_response = element_to_dict_with_children(product_enquiry_response)
         for k,v in product_enquiry_response.items():
             if isinstance(v,bytes):
                 product_enquiry_response[k] = v.decode('utf-8')
         print(product_enquiry_response)
+        print("========================")
+    else:
+        print(f"Response: {request.text}")
+        print("=========================")
 
 
 print("Please enter the tag for the request: ")
